fc7c4853ff20a2edd8e1e4d989d9b316d2c7d732
Nitheesh D R <nitheeraj1@gmail.com>
2026-02-01 18:08:20 +0530

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,51 @@
+import CompanionCard from "@/components/CompanionCard"
+import CompanionList from "@/components/CompanionList"
+import CTA from "@/components/CTA"
+import { recentSessions } from "@/constants"
+
 const page = () => {
   return (
-    <div>page</div>
+    <main>
+      <h1>Popular Companions</h1>
+
+      <section className="home-section">
+        <CompanionCard
+          id="1"
+          subject="science"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          duration={45}
+          color="#E5D0FF"
+        />
+
+        <CompanionCard
+          id="2"
+          subject="maths"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          duration={30}
+          color="#FFDA6E"
+        />
+
+        <CompanionCard
+          id="3"
+          subject="language"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature"
+          duration={30}
+          color="#BDE7FF"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionList 
+        title = "Recently completed sessions"
+        companions={recentSessions}
+        className="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
   )
 }
 

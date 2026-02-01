9d3fa8524d73b72e63658653b8263f264e35495b
Nitheesh D R <nitheeraj1@gmail.com>
2026-02-01 15:51:20 +0530

    Project init

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className='text-2xl underline'>Nitheesh</h1>
+      <Button>Click me</Button>
+    </div>
+    
+  )
+}
+
+export default Page
